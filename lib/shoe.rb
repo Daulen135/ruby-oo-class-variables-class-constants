e5391e158b84573f29a3e4c9644cc2aea7d8d13d
Daulen135 <daulenz@gmail.com>
2021-03-06 09:14:39 -0600

    commit tree 22d9c675f9e1d490defd5320033c73513b035c51

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,14 +1,68 @@
+require 'pry'
+
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
+  BRANDS = []
+
   def initialize(brand)
     @brand = brand
+    BRANDS << brand unless BRANDS.any? { |b| b == brand }
   end
+
+
 
   def cobble
     self.condition = "new"
     puts "Your shoe is as good as new!"
   end
-
 end
+
+
+
+
+
+
+
+
+# class Shoe
+#   attr_accessor :color, :size, :material, :condition
+#   attr_reader :brand
+#   BRANDS = []
+#   @@all=[]
+
+  
+#   def initialize(brand)
+#     @brand = brand
+   
+#   end
+  
+#   def brands=(brand)
+    
+#     @brand = brand
+    
+#     Shoe::BRANDS << brand 
+#     #unless BRANDS.any? {|b|b==genre}
+    
+#   end
+#   # binding.pry
+#   # q
+#   # exit
+
+    
+    
+#   #  BRANDS << brand unless BRADNDS.any? {|b|b == brand}
+
+#   def cobble
+#     self.condition = "new"
+#     puts "Your shoe is as good as new!"
+#   end
+
+  
+  
+#    def self.all
+#     @@all
+#    end
+
+# end
